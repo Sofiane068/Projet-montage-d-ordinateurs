77deb23607fdc3ddef8c5bb600d69bccd1624b93
Sofiane Hachemaoui <sofiane068@hotmail.fr>
2022-02-18 16:11:45 +0100

    ajout message erreur sur la page connexion

--- index.php
+++ index.php
@@ -14,50 +14,48 @@ if (isset($_POST['email']) && isset($_POST['key'])) {
     $utilisateur = $sth->fetch();
 
     if ($utilisateur == null) {
-        echo 'email invalide';
-        
-    }elseif ($utilisateur->getAddresseMail() == 'concepteur@cldl.com' && $utilisateur !== null) {
+?> <div class="alert alert-danger" role="alert">information saisie invalide</div>
+    <?php
+    } elseif ($utilisateur->getAddresseMail() == 'concepteur@cldl.com' && $utilisateur !== null) {
         header("Location: concepteur.php");
     } elseif ($utilisateur->getAddresseMail() == 'monteur@cldl.com' && $utilisateur !== null) {
         header("Location: monteur.php");
     }
-
- 
 }
 
-?>
+    ?>
 
 
 
-<section id="login">
-    <div class="container">
-        <div class="row">
-            <div class="col-xs-12">
-                <div class="form-wrap mt-5">
-                    <h1>Connectez-vous </h1>
-                    <form role="form" action="" method="post" id="login-form" autocomplete="off">
-                        <div class="form-group">
-                            <label for="email" class="sr-only">Adresse mail </label>
-                            <input type="email" name="email" id="email" class="form-control" placeholder="idconcepteur@cldl.com">
-                        </div>
-                        <div class="form-group">
-                            <label for="key" class="sr-only">Mot de passe </label>
-                            <input type="password" name="key" id="key" class="form-control" placeholder="Mot de passe">
-                        </div>
-                        <div class="d-flex justify-content-center">
-                            <input type="submit" id="btn-login" class="btn btn-custom btn-lg btn-block " value="Connexion">
-                        </div>
-                    </form>
-                    <hr>
+    <section id="login">
+        <div class="container">
+            <div class="row">
+                <div class="col-xs-12">
+                    <div class="form-wrap mt-5">
+                        <h1>Connectez-vous </h1>
+                        <form role="form" action="" method="post" id="login-form" autocomplete="off">
+                            <div class="form-group">
+                                <label for="email" class="sr-only">Adresse mail </label>
+                                <input type="email" name="email" id="email" class="form-control" placeholder="idconcepteur@cldl.com">
+                            </div>
+                            <div class="form-group">
+                                <label for="key" class="sr-only">Mot de passe </label>
+                                <input type="password" name="key" id="key" class="form-control" placeholder="Mot de passe">
+                            </div>
+                            <div class="d-flex justify-content-center">
+                                <input type="submit" id="btn-login" class="btn btn-custom btn-lg btn-block " value="Connexion">
+                            </div>
+                        </form>
+                        <hr>
+                    </div>
                 </div>
-            </div> 
-        </div> 
-    </div> 
-</section>
+            </div>
+        </div>
+    </section>
 
 
 
 
-<?php
-require_once 'footer.php';
-?>
+    <?php
+    require_once 'footer.php';
+    ?>
