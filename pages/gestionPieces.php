5493655f5fd6f5233a63c04865d3bb18d6236cad
Sofiane Hachemaoui <sofiane068@hotmail.fr>
2022-02-24 21:52:53 +0100

    ajout dun select

--- pages/gestionPieces.php
+++ pages/gestionPieces.php
@@ -13,6 +13,24 @@ $pieces = $sth->fetchAll();
 
 <div class="container">
     <div class="row mt-3">
+        <div class="col-2">
+            <h5>Trier par :</h5>
+        </div>
+        <div class="col-6">
+            <select class="form-select" aria-label="Default select example">
+                <option value="1">nom</option>
+                <option value="2">marque</option>
+                <option value="3">quantite en stock</option>
+                <option value="4">prix</option>
+                <option value="5">nombre de modèles créés</option>
+                <option value="6">date d'ajout</option>
+            </select>
+        </div>
+        <div class="col">
+            <a href="" class="btn btn-primary">Validez</a>
+        </div>
+    </div>
+    <div class="row mt-3">
         <table class="table table-dark">
             <thead>
                 <tr>
@@ -20,7 +38,7 @@ $pieces = $sth->fetchAll();
                     <th scope="col">marque</th>
                     <th scope="col">quantite en stock</th>
                     <th scope="col">prix</th>
-                    <th scope="col">nombre de modèles creer</th>
+                    <th scope="col">nombre de modèles créés</th>
                     <th scope="col">catégorie</th>
                 </tr>
             </thead>
