c8d0a249af7674eb4af6e2be3d1f317a8c3c16dd
Sofiane Hachemaoui <sofiane068@hotmail.fr>
2022-02-24 09:17:42 +0100

    affichage commentaire

--- class/Commentaire.php
+++ class/Commentaire.php
@@ -1,47 +1,109 @@
 <?php
-class Commentaire{
-   protected DateTime $dateCommentaire;
-   protected string $texte;
-   protected bool $repondue;
+class Commentaire
+{
+    protected ?string $dateCommentaire;
+    protected string $texte;
+    protected bool $repondue;
+    protected ?string $adresseMail_1;
+    protected ?string $adresseMail;
 
-public function __construct(DateTime $dateCommentaire, string $texte, bool $repondue) 
+
+    public function __construct(?string $dateCommentaire = null, string $texte = "", bool $repondue = false, string $adresseMail_1="", string $adresseMail="")
+    {
+        if (!empty($dateCommentaire)) {
+            $this->dateCommentaire = $dateCommentaire;
+        }
+        if (!empty($texte)) {
+            $this->texte = $texte;
+        }
+        if (!empty($repondue)) {
+            $this->repondue = $repondue;
+        }
+        if (!empty($adresseMail_1)) {
+            $this->adresseMail_1 = $adresseMail_1;
+        }
+        if (!empty($adresseMail)) {
+            $this->adresseMail = $adresseMail;
+        }
+    }
+    ///////////
+    public function getdate(): DateTime
+    {
+        if (is_string($this->dateCommentaire)) {
+            return new DateTime($this->dateCommentaire);
+        }
+        return $this->dateCommentaire;
+    }
+
+    public function setdate(DateTime $dateCommentaire)
     {
         $this->dateCommentaire = $dateCommentaire;
-        $this->texte = $texte;
-        $this->repondue = $repondue;
-
+        return $this;
     }
-///////////
-public function getdate(){
-    return $this->dateCommentaire;
-}
 
-public function setdate(DateTime $dateCommentaire){
-$this->dateCommentaire= $dateCommentaire;
-return $this;
-}
+    ////////////
+    public function gettexte()
+    {
+        return $this->texte;
+    }
 
-////////////
-public function gettexte(){
-    return $this->texte;
-}
+    public function settexte(string $texte)
+    {
+        $this->text = $texte;
+        return $this;
+    }
 
-public function settexte(string $texte){
-$this->text= $texte;
-return $this;
-}
-
-/////////////
+    /////////////
 
 
-public function getreponse(){
-    return $this->repondue;
-}
+    public function getreponse()
+    {
+        return $this->repondue;
+    }
 
-public function setreponse(bool $repondue){
-$this->repondue= $repondue;
-return $this;
-}
+    public function setreponse(bool $repondue)
+    {
+        $this->repondue = $repondue;
+        return $this;
+    }
 
+    /**
+     * Get the value of adresseMail_1
+     */ 
+    public function getAdresseMail_1()
+    {
+        return $this->adresseMail_1;
+    }
 
+    /**
+     * Set the value of adresseMail_1
+     *
+     * @return  self
+     */ 
+    public function setAdresseMail_1(string $adresseMail_1)
+    {
+        $this->adresseMail_1 = $adresseMail_1;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of adresseMail
+     */ 
+    public function getAdresseMail()
+    {
+        return $this->adresseMail;
+    }
+
+    /**
+     * Set the value of adresseMail
+     *
+     * @return  self
+     */ 
+    public function setAdresseMail(string $adresseMail)
+    {
+        $this->adresseMail = $adresseMail;
+
+        return $this;
+    }
 };
--- pages/concepteur.php
+++ pages/concepteur.php
@@ -1,2 +1,91 @@
-
 <p> Bienvenue sur la partie concepteur </p>
+<?php
+
+$sth = $connection->prepare("SELECT * FROM commentaire  ORDER BY dateCommentaire DESC");
+
+$sth->setFetchMode(PDO::FETCH_CLASS, Commentaire::class);
+
+$sth->execute();
+
+$commentaires = $sth->fetchAll();
+
+
+?>
+
+<div class="container">
+    <?php foreach ($commentaires as $key => $commentaire) {
+    ?>
+
+        <div class="row">
+            <div class="col-8">
+                <div class="card card-white p-4 mb-2">
+                    <div class="post-heading">
+                        <div class="float-left image">
+                            <img src="http://bootdey.com/img/Content/user_1.jpg" class="img-circle avatar" alt="user profile image">
+                        </div>
+                        <div class="float-left meta">
+                            <div class="title h5">
+                                <a href="#"><b><?php if ($commentaire->getAdresseMail() != null) {
+                                                    echo $commentaire->getAdresseMail();
+                                                } else {
+                                                    echo $commentaire->getAdresseMail_1();
+                                                } ?></b></a>
+                                a commenté
+                            </div>
+                            <h6 class="text-muted time"><?php echo $commentaire->getdate()->format('d/m/Y'); ?></h6>
+                        </div>
+                    </div>
+                    <div class="post-description">
+                        <p><?php echo $commentaire->gettexte(); ?></p>
+
+                    </div>
+                    <div>
+                        <?php if ($commentaire->getreponse() == true) { ?>
+                            <p class="text-muted time">répondu</p>
+                        <?php } else { ?>
+                            <p class="text-muted time">non répondu</p>
+                        <?php } ?>
+                    </div>
+
+                </div>
+            </div>
+            <!-- /////////////// -->
+            <?php if ($commentaire->getreponse()==true){ ?>
+            <div class="col-4">
+                <div class="card card-white p-4 mb-2">
+                    <div class="post-heading">
+                        <div class="float-left image">
+                            <img src="http://bootdey.com/img/Content/user_1.jpg" class="img-circle avatar" alt="user profile image">
+                        </div>
+                        <div class="float-left meta">
+                            <div class="title h5">
+                                <a href="#"><b><?php if ($commentaire->getAdresseMail() != null) {
+                                                    echo $commentaire->getAdresseMail();
+                                                } else {
+                                                    echo $commentaire->getAdresseMail_1();
+                                                } ?></b></a>
+                                a commenté
+                            </div>
+                            <h6 class="text-muted time"><?php echo $commentaire->getdate()->format('d/m/Y'); ?></h6>
+                        </div>
+                    </div>
+                    <div class="post-description">
+                        <p><?php echo $commentaire->gettexte(); ?></p>
+
+                    </div>
+                    <div>
+                        <?php if ($commentaire->getreponse() == true) { ?>
+                            <p class="text-muted time">répondu</p>
+                        <?php } else { ?>
+                            <p class="text-muted time">non répondu</p>
+                        <?php } ?>
+                    </div>
+
+                </div>
+            </div>
+            <?php }; ?>
+
+
+        </div>
+    <?php }; ?>
+</div>
