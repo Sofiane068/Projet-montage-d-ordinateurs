5d2a6ee289eb7a7d2a3def28e96b69b843320b62
Sofiane Hachemaoui <sofiane068@hotmail.fr>
2022-02-24 10:26:01 +0100

    finalisation styleCommentaire

--- header.php
+++ header.php
@@ -14,6 +14,8 @@ require_once 'includes/db.php';
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>CLDL</title>
+    <link href="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.2/css/bootstrap-combined.no-icons.min.css" rel="stylesheet">
+    <link href="//netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.css" rel="stylesheet">
     <link href=" https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link href="style.css" rel="stylesheet">
 </head>
--- pages/concepteur.php
+++ pages/concepteur.php
@@ -14,78 +14,94 @@ $commentaires = $sth->fetchAll();
 
 <div class="container">
     <?php foreach ($commentaires as $key => $commentaire) {
+
+        if ($commentaire->getAdresseMail_1() != null) {
     ?>
 
-        <div class="row">
-            <div class="col-8">
-                <div class="card card-white p-4 mb-2">
-                    <div class="post-heading">
-                        <div class="float-left image">
-                            <img src="http://bootdey.com/img/Content/user_1.jpg" class="img-circle avatar" alt="user profile image">
-                        </div>
-                        <div class="float-left meta">
-                            <div class="title h5">
-                                <a href="#"><b><?php if ($commentaire->getAdresseMail() != null) {
-                                                    echo $commentaire->getAdresseMail();
-                                                } else {
-                                                    echo $commentaire->getAdresseMail_1();
-                                                } ?></b></a>
-                                a commenté
+            <div class="row">
+                <div class="col-6">
+                    <div class="card card-white p-4 mb-2">
+                        <div class="post-heading">
+                            <div class="float-left image">
+                                <img src="http://bootdey.com/img/Content/user_1.jpg" class="img-circle avatar" alt="user profile image">
                             </div>
-                            <h6 class="text-muted time"><?php echo $commentaire->getdate()->format('d/m/Y'); ?></h6>
-                        </div>
-                    </div>
-                    <div class="post-description">
-                        <p><?php echo $commentaire->gettexte(); ?></p>
-
-                    </div>
-                    <div>
-                        <?php if ($commentaire->getreponse() == true) { ?>
-                            <p class="text-muted time">répondu</p>
-                        <?php } else { ?>
-                            <p class="text-muted time">non répondu</p>
-                        <?php } ?>
-                    </div>
-
-                </div>
-            </div>
-            <!-- /////////////// -->
-            <?php if ($commentaire->getreponse()==true){ ?>
-            <div class="col-4">
-                <div class="card card-white p-4 mb-2">
-                    <div class="post-heading">
-                        <div class="float-left image">
-                            <img src="http://bootdey.com/img/Content/user_1.jpg" class="img-circle avatar" alt="user profile image">
-                        </div>
-                        <div class="float-left meta">
-                            <div class="title h5">
-                                <a href="#"><b><?php if ($commentaire->getAdresseMail() != null) {
-                                                    echo $commentaire->getAdresseMail();
-                                                } else {
-                                                    echo $commentaire->getAdresseMail_1();
-                                                } ?></b></a>
-                                a commenté
+                            <div class="float-left meta">
+                                <div class="title h5">
+                                    <a href="#"><b><?php if ($commentaire->getAdresseMail() != null) {
+                                                        echo $commentaire->getAdresseMail();
+                                                    } else {
+                                                        echo $commentaire->getAdresseMail_1();
+                                                    } ?></b></a>
+                                    a commenté
+                                </div>
+                                <h6 class="text-muted time"><?php echo $commentaire->getdate()->format('d/m/Y'); ?></h6>
                             </div>
-                            <h6 class="text-muted time"><?php echo $commentaire->getdate()->format('d/m/Y'); ?></h6>
+                        </div>
+                        <div class="post-description">
+                            <p><?php echo $commentaire->gettexte(); ?></p>
+
+                        </div>
+                        <div>
+                            <?php if ($commentaire->getreponse() == true) { ?>
+                                <p class="text-muted time">répondu</p>
+                            <?php } else { ?>
+                                <p class="text-muted time">non répondu</p>
+                            <?php } ?>
+                        </div>
+
+                    </div>
+                </div>
+                <!-- /////////////// -->
+                <?php if ($commentaire->getreponse() == true) { ?>
+                    <div class='col-1 '>
+                        <h1 class="ms-3 mt-5"><i class="icon-arrow-right fa-5x"></i></h1>
+                    </div>
+                    <div class="col-5">
+                        <div class="card card-white p-4 mb-2">
+                            <div class="post-heading">
+                                <div class="float-left image">
+                                    <img src="http://bootdey.com/img/Content/user_1.jpg" class="img-circle avatar" alt="user profile image">
+                                </div>
+                                <div class="float-left meta">
+                                    <div class="title h5">
+                                        <a href="#"><b><?php if ($key == 4 || $key == 7) {
+                                                            echo $commentaires[3]->getAdresseMail();
+                                                        } ?></b></a>
+                                        a commenté
+                                    </div>
+                                    <h6 class="text-muted time">
+                                        <?php if ($key == 4) {
+                                            echo $commentaires[3]->getdate()->format('d/m/Y');
+                                        } elseif ($key == 7) {
+                                            echo $commentaires[6]->getdate()->format('d/m/Y');
+                                        } ?>
+                                    </h6>
+                                </div>
+                            </div>
+                            <div class="post-description">
+                                <p>
+                                    <?php if ($key == 4) {
+                                        echo $commentaires[3]->gettexte();
+                                    } elseif ($key == 7) {
+                                        echo $commentaires[6]->gettexte();
+                                    } ?>
+                                </p>
+
+                            </div>
+                            <div>
+                                <?php if ($commentaire->getreponse() == true) { ?>
+                                    <p class="text-muted time">répondu</p>
+                                <?php } else { ?>
+                                    <p class="text-muted time">non répondu</p>
+                                <?php } ?>
+                            </div>
+
                         </div>
                     </div>
-                    <div class="post-description">
-                        <p><?php echo $commentaire->gettexte(); ?></p>
+                <?php }; ?>
 
-                    </div>
-                    <div>
-                        <?php if ($commentaire->getreponse() == true) { ?>
-                            <p class="text-muted time">répondu</p>
-                        <?php } else { ?>
-                            <p class="text-muted time">non répondu</p>
-                        <?php } ?>
-                    </div>
 
-                </div>
             </div>
-            <?php }; ?>
-
-
-        </div>
-    <?php }; ?>
+    <?php }
+    }; ?>
 </div>
